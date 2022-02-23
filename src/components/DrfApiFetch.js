5ed49134bb316dcad6797d0da5707963fb51ca9c
youichi Ishigaki <deepestred1968@gmail.com>
2022-02-23 16:45:12 +0900

    更新処理を追加

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const DrfApiFetchById = () => {
 	const [tasks, setTasks] = useState([]);
 	const [selectedTask, setSelectedTask] = useState([]);
+	const [editedTask, setEditedTask] = useState({ id: "", title: "" });
 	const [id, setId] = useState(1);
 	const token = "8edb985fa2e003cf8cc7b81a1e392d3e91a32786";
 
@@ -21,7 +22,7 @@ const DrfApiFetchById = () => {
 
 	const deleteTask = (taskId) => {
 		axios
-			.delete(`http://127.0.0.1:8000/api/tasks/${taskId}`, {
+			.delete(`http://127.0.0.1:8000/api/tasks/${taskId}/`, {
 				headers: {
 					Authorization: `Token ${token}`,
 				},
@@ -30,7 +31,50 @@ const DrfApiFetchById = () => {
 				console.log(res);
 				setTasks(tasks.filter((task) => task.id !== taskId));
 				setSelectedTask([]);
+				if (taskId === editedTask.id) {
+					setEditedTask({ id: "", title: "" });
+				}
 			});
+	};
+	const createTask = (task) => {
+		const data = {
+			title: task.title,
+		};
+		axios
+			.post(`http://127.0.0.1:8000/api/tasks/`, data, {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Token ${token}`,
+				},
+			})
+			.then((res) => {
+				setTasks([...tasks, res.data]);
+				setEditedTask({ id: "", title: "" });
+			});
+	};
+
+	const editTask = (task) => {
+		axios
+			.put(`http://127.0.0.1:8000/api/tasks/${task.id}/`, task, {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Token ${token}`,
+				},
+			})
+			.then((res) => {
+				setTasks(
+					tasks.map((task) => (task.id === editedTask.id ? res.data : task))
+				);
+				setSelectedTask({ id: "", title: "" });
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const handleInputChange = () => (evt) => {
+		const value = evt.target.value;
+		const name = evt.target.name;
+		setEditedTask({ ...editedTask, [name]: value });
 	};
 	useEffect(() => {
 		axios
@@ -57,6 +101,14 @@ const DrfApiFetchById = () => {
 						>
 							<span className="fa fa-trash-alt"></span>
 						</button>
+						<button
+							type="button"
+							onClick={() => {
+								setEditedTask(task);
+							}}
+						>
+							<span className="fa fa-pen"></span>
+						</button>
 					</li>
 				))}
 			</ul>
@@ -79,6 +131,21 @@ const DrfApiFetchById = () => {
 				<h3>
 					{selectedTask.title}:{selectedTask.id}
 				</h3>
+			</div>
+			<div>
+				<input
+					type="text"
+					name="title"
+					value={editedTask.title}
+					onChange={handleInputChange()}
+					placeholder="new Task"
+					required
+				/>
+				{editedTask.id ? (
+					<button onClick={() => editTask(editedTask)}>Update</button>
+				) : (
+					<button onClick={() => createTask(editedTask)}>Create</button>
+				)}
 			</div>
 		</div>
 	);
